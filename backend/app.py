c092c2e74e76422e700146dd465ee4c345e0d71a
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2025-03-15 11:50:25 +1300

    Create requirements.txt, add stuff to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,35 @@
 from flask import Flask
 
+import sqlite3
+
 app = Flask(__name__)
+settings = Settings()
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+@app.route("/api")
+def api_docs() -> str:
+    return "Nothing Yet!"
+
+
+@app.get("/api/img_gen/")
+def prompt() -> str:
+    return "foo"
+
+
+# User
+@app.post("/api/user/login")
+def register(id, user):
+    return
+
+@app.post("/api/user/login")
+def login(id, user):
+    return
+
+@app.get("/api/user/{user}/about")
+def about(user):
+    return
+
+@app.get("/api/user")
