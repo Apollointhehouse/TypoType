f5c138de727338ec881a39ab207b34ec6ce6a7e5
Jason Xavier <jasxavier7@gmail.com>
2025-03-15 18:03:41 +1300

    added progress tracking and prevented space spamming

--- frontend/src/components/gameinteface-components/GameInterface.vue
+++ frontend/src/components/gameinteface-components/GameInterface.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
 import { ref, computed, onMounted, onBeforeUnmount} from 'vue';
 import {processData} from '../../utils/DataProcessor';
-import {DummyPromptList} from '../../enums/DummyPrompt';
+import {DummyPrompt} from '../../enums/DummyPrompt';
 import WordList from './WordList/WordList.vue';
 import {WordModel} from '../../models/Word';
+import {WordState} from '../../enums/enums';
 
-const promptList = DummyPromptList;
-// Reactive variable to store the current word being typed
 const userInput = ref<string>("");
+const prompt = {value: DummyPrompt};
 
 // List to store completed words
 const data = computed<WordModel[]>(() => {
+  const promptList = prompt.value.split(" ");
   return processData(userInput.value.split(" "), promptList);
 });
 
+const progress = computed<{current: number, total: number}>(() => {
+  const userInputList = userInput.value.split(" ");
+  const promptList = prompt.value.split(" ");
+  return {current: userInputList.length - 1, total: promptList.length};
+});
+
 // Function to handle input changes
+let lastInput = " ";
 function handleInput(event: KeyboardEvent) {
   const key = event.key;
 
-  if (key === 'Backspace') {
+  if (key === " " && lastInput === " ") {
+    // prevent consecutive spaces
+    return;
+  } else if (key === 'Backspace') {
     userInput.value = userInput.value.slice(0, -1);
   } else if (/^[a-zA-Z0-9\s.,!?;:'"(){}[\]<>@#$%^&*-_=+|\\/~`€¢€]$/.test(key)) {
     // Add alphanumeric characters (letters and numbers) to the current word
     userInput.value += key;
+    lastInput = key;
   }
 }
 
@@ -39,6 +51,7 @@ onBeforeUnmount(() => {
   <div>
     <!-- Display the current word and completed words -->
     <!-- <div>{{ userInput }}</div> -->
+    <div>{{ progress.current }} / {{ progress.total }}</div>
     <WordList :value="data" :key="userInput.length"/>
   </div>
 </template>
