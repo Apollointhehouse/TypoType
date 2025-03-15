d32f97be7b25c007de137e6338a90f6ef6654d8d
foopod <jonathonshields@gmail.com>
2025-03-15 13:33:28 +1300

    integrate prompt fetch and keymap get

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
-
+from flask import Flask, request, Response
 import db
+import os
+import secrets
+import json
+
 from User import User
+from keymap import get_keymap
 
 app = Flask(__name__)
 
@@ -10,6 +14,19 @@ db.setup_db()
 @app.route("/")
 def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+@app.route("/prompt", methods=["GET"])
+def get_random_prompt():
+    files = os.listdir('prompts')
+    file = secrets.choice(files)
+    with open(f'prompts/{file}', 'r') as f:
+        output = f.read()
+    return Response(output, status=200)
+
+@app.route("/keymap", methods=["GET"])
+def gen_keymap():
+    keymap = get_keymap()
+    return Response(json.dumps(keymap), status=200)
 
 @app.route("/api")
 def api_docs() -> str:
