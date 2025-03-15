958bc773d7199b3103977e458ac7433f8c16e5ce
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2025-03-15 15:41:57 +1300

    broken db shit

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,21 @@
+import sqlite3
+from datetime import datetime
+
 from flask import Flask, request, Response
 import db
 import os
 import secrets
 import json
 
-from User import User
+from User import User, createUser, getUser
 from keymap import get_keymap
 
 app = Flask(__name__)
 
-db.setup_db()
+con = sqlite3.connect("data.db")
+cur = con.cursor()
+db.setup_db(con, cur)
+
 
 @app.route("/")
 def hello_world() -> str:
@@ -30,29 +36,19 @@ def get_keymap():
 
 @app.route("/scores", methods=["POST"])
 def post_score():
+    data = request.get_json()
+
+
+
+    res = cur.execute(f"INSERT INTO scores values ({data.get('id')}, {data.get('score')})")
+    con.commit()
+    print(res)
+
     return Response(request.get_json(), status=201)
 
 @app.route("/highscores", methods=["GET"])
 def get_highscores():
+    print(cur.execute("SELECT * FROM scores").fetchall())
     return Response(json.dumps([{"name": 'Bob', "wpm":1}, {"name": 'Jack', "wpm":3}, {"name": 'Fern', "wpm":32}, {"name": 'Asley', "wpm":13}]), status=201)
 
 # User
-@app.post("/api/user/login")
-def register(id, user):
-
-    return
-
-@app.post("/api/user/login")
-def login(id, user):
-    return
-
-@app.post("/api/user")
-def about():
-    data = request.json
-    id = data.get("id")
-
-    user = User(id)
-
-    return " ".join([user.name, user.score, user.timestamp])
-
-# @app.get("/api/user")
